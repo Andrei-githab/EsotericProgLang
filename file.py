efb26b0441b5da6f498654f8ea293417ecb40116
Andru-hub <67545441+Andru-hub@users.noreply.github.com>
2022-07-18 11:52:39 +0300

    Update file.py

--- file.py
+++ file.py
@@ -1,20 +1,18 @@
 class MyCompeler:
     def MyPrint(self, str):
         res = str
-        print(res[res.find("spwninfo:") + 10:])
+        print(res[10:])
 
     def MyScan(self):
         input()
 
 
-with open("input.txt", "r", encoding='UTF-8') as fin:
-    compeler = MyCompeler()
-    inputstr = fin.read()
-    for string in inputstr:
-        if inputstr.find('spwninfo:') != -1:
-            compeler.MyPrint(inputstr)
-            break
+compeler = MyCompeler()
 
-        if inputstr.find('plantedinfo:') != -1:
+with open("input.txt", "r", encoding='UTF-8') as file:
+    for string in file:
+        if string.find('spwninfo:') != -1:
+            compeler.MyPrint(string)
+
+        elif string.find('plantedinfo:') != -1:
             compeler.MyScan()
-            break
